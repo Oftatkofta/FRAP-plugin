b13b7d455fff8c319395347636ae7bdcc0f2f1eb
Oftatkofta <jens_e@Jenss-MacBook-Pro.local>
2015-04-13 00:54:56 +0200

    Tracking bug fixed
    An annoying tracking bug was fixed.
    Means now recorded in both channels and plotted
    Test still super broken

--- PML_buddy_test2.py
+++ PML_buddy_test2.py
@@ -9,6 +9,9 @@ from ij import IJ, ImagePlus, ImageStack
 import ij.process.ImageStatistics as ImageStatistics
 from ij.measure import Measurements as Measurements
 from ij import IJ as IJ
+import java.awt.Color as Color
+from ij.gui import Plot as Plot
+from ij.gui import PlotWindow as PlotWindow
 
 def roiCenterer(ip, roi, cal):
     """Arguments: ip:ImageProcessor, roi:Region of intrest, cal:calibration of ip.
@@ -37,7 +40,7 @@ channel_to_track=imp.getChannel()
 start_frame=imp.getFrame()
 n_frames=imp.getNFrames()
 stack_to_track=1
-no_of_centerings=10
+no_of_centerings=6
 
 
 # Get the ROIs
@@ -64,20 +67,16 @@ for i in range(start_frame, n_frames+1):
     #Do the Roi centering the desired number of times
     for i in range (no_of_centerings):
         roi=roiCenterer(ip, roi, cal)
-        roi_x=roi_1.getXBase()
-        roi_y=roi_1.getYBase()
-    
-    ip2=ip.duplicate()
-    ip2.setColor(ip.maxValue())
-    ip2.draw(roi)
-    stack2.addSlice(ip2)
-    
+        roi_x=roi.getXBase()
+        roi_y=roi.getYBase()
+    ip.setRoi(roi)
+
     
     #Get Channel 1 IP and apply the centered roi
-    ip = stack.getProcessor(imp.getStackIndex(1,stack_to_track,i))
-    ip.setRoi(roi)
+    ip1 = stack.getProcessor(imp.getStackIndex(1,stack_to_track,i))
+    ip1.setRoi(roi)
     # Make a measurement in it and record the stats
-    stats = ImageStatistics.getStatistics(ip, ImageStatistics.CENTER_OF_MASS, cal)
+    stats = ImageStatistics.getStatistics(ip1, ImageStatistics.CENTER_OF_MASS, cal)
     x=cal.getRawX(stats.xCenterOfMass)
     y=cal.getRawY(stats.yCenterOfMass)
     means1.append(stats.mean)
@@ -85,18 +84,24 @@ for i in range(start_frame, n_frames+1):
     c1y.append(y)
     
     #Get Channel 2 IP, apply the centered roi and do the analysis
-    ip = stack.getProcessor(imp.getStackIndex(2,stack_to_track,i))
+    ip2 = stack.getProcessor(imp.getStackIndex(2,stack_to_track,i))
     #roi = OvalRoi(roi_x, roi_y, roi_w, roi_h)
-    ip.setRoi(roi)
-    stats = ImageStatistics.getStatistics(ip, ImageStatistics.CENTER_OF_MASS, cal)
+    ip2.setRoi(roi)
+    stats = ImageStatistics.getStatistics(ip2, ImageStatistics.CENTER_OF_MASS, cal)
     x=cal.getRawX(stats.xCenterOfMass)
     y=cal.getRawY(stats.yCenterOfMass)
     means2.append(stats.mean)
     c2x.append(x)
     c2y.append(y)
 
+    ip2=ip.duplicate()
+    ip2.setColor(ip.maxValue())
+    ip2.draw(roi)
+    stack2.addSlice(ip2)
+
     
-im2 = ImagePlus(imp.getTitle()+'_Processed', stack2)
+im2 = ImagePlus(title+'_Processed', stack2)
+im2.setCalibration(cal)
 im2.show()
 
 IJ.run("Clear Results")
@@ -114,3 +119,25 @@ for i in range(len(means1)):
 
 rt.disableRowLabels()
 rt.show(title)
+
+maxc1=max(means1)
+maxc2=max(means2)
+if maxc1>maxc2:
+    plotlim = maxc1
+else:
+    plotlim = maxc2
+
+time=range(0,len(means1))
+plot = Plot("Traced intensity curve for " + imp.getTitle(), "Time", "Mean intensity", [], [])
+plot.setLimits(1, n_frames+1, 0, plotlim );
+plot.setLineWidth(2)
+
+plot.setColor(Color.GREEN)
+plot.addPoints(time, means1, Plot.LINE)
+
+plot.setColor(Color.RED)
+plot.addPoints(time, means2, Plot.LINE)
+ 
+plot.setColor(Color.black)
+plot_window =  plot.show()
+
