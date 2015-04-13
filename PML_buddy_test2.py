78e58d05fd81908e30210fdc89ed13b235f207a6
Oftatkofta <jens_e@Jenss-MacBook-Pro.local>
2015-04-14 00:21:22 +0200

    Options dialog implemented
    V 0.1!

--- PML_buddy_test2.py
+++ PML_buddy_test2.py
@@ -1,4 +1,4 @@
-import ij.gui
+#import ij.gui
 import ij.gui.Roi as Roi
 import ij.gui.OvalRoi as OvalRoi
 import ij.measure
@@ -12,6 +12,42 @@ from ij import IJ as IJ
 import java.awt.Color as Color
 from ij.gui import Plot as Plot
 from ij.gui import PlotWindow as PlotWindow
+from ij.gui import GenericDialog
+
+
+def setupDialog(imp):
+
+    gd = GenericDialog("PML Buddy options")
+    gd.addMessage("Welcome to PML Buddy 0.1, you are analyzing: "+imp.getTitle())
+    calibration = imp.getCalibration()
+
+    if calibration.frameInterval is None:
+        default_interval = 0.00
+    else:
+        default_interval=calibration.frameInterval
+
+    gd.addNumericField("Frame interval (s):", default_interval, 3)  # show 3 decimals    
+    gd.addMessage("(optional)")
+    channels = [str(ch) for ch in range(1, imp.getNChannels()+1)]  
+    gd.addChoice("Channel to track:", channels, channels[1])
+
+    roichoises = ['Current active Roi','First Roi in RoiManager']    
+    gd.addChoice('Roi to use for tracking: (not implemented yet)', roichoises, roichoises[1])
+    
+    gd.addNumericField("Number of ROI centerings to perform per frame (3-6 is generally ok):", 6, 0)
+    gd.addSlider("Start tracking at frame:", 1, imp.getNFrames(), imp.getFrame())
+    gd.addSlider("Stop tracking at frame:", 1, imp.getNFrames(), imp.getNFrames())
+    gd.addCheckbox("Display tracking in new window", True)
+    gd.addCheckbox("Show plot", True)
+    gd.addCheckbox("Show results table", True)
+    
+    gd.showDialog()  
+	  
+    if gd.wasCanceled():  
+        IJ.log("User canceled dialog!")  
+        return
+
+    return gd
 
 def roiCenterer(ip, roi, cal):
     """Arguments: ip:ImageProcessor, roi:Region of intrest, cal:calibration of ip.
@@ -29,25 +65,51 @@ def roiCenterer(ip, roi, cal):
     roi = OvalRoi(roi_x, roi_y, roi_w, roi_h)
     return roi
 
+#Start by getting the active image window
 imp = WindowManager.getCurrentImage()
+cal = imp.getCalibration()
+
+# Run the setupDialog and read out the options
+gd=setupDialog(imp)  	
+
+frame_interval = gd.getNextNumber()
+channel_to_track = int(gd.getNextChoice())
+roi_to_use = gd.getNextChoice()  
+no_of_centerings = gd.getNextNumber()
+start_frame = int(gd.getNextNumber())
+stop_frame = int(gd.getNextNumber())
+
+if (start_frame > stop_frame):
+    IJ.showMessage("Start frame > Stop frame!")
+    raise RuntimeException("Start frame > Stop frame!")
+
+showTrackFlag=gd.getNextBoolean()
+showPlotFlag=gd.getNextBoolean()
+showResultsFlag=gd.getNextBoolean()
+
+    
+#Set the frame interval in calibration
+    
+cal.frameInterval = frame_interval
+imp.setCalibration(cal)
+    
+
+  	   
+
+
 stack = imp.getImageStack()
 stack2 = imp.createEmptyStack()
-cal = imp.getCalibration()
 title = imp.getTitle()
 n_channels = imp.getNChannels()
-
-channel_to_track=imp.getChannel()
-start_frame=imp.getFrame()
-n_frames=imp.getNFrames()
 stack_to_track=1
-no_of_centerings=6
+
 
 
 # Get the ROIs
 roi_manager = RoiManager.getInstance()
 roi_list    = roi_manager.getRoisAsArray()
- 
-# We assume first one is FRAP roi, the 2nd one is normalizing roi.
+
+# We will use the first ROI in the Roi manager for now.
 roi_1 = roi_list[0];
 
 roi_x=roi_1.getXBase()
@@ -59,7 +121,7 @@ roi_h=roi_1.getFloatHeight()
 c1x, c1y, c2x, c2y, means1, means2=[],[],[],[],[],[]
 
 #loop through the frames that you want to track
-for i in range(start_frame, n_frames+1):
+for i in range(start_frame, stop_frame+1):
     # Get the imageProcessor of the channel to track and at the current frame
     ip = stack.getProcessor(imp.getStackIndex(channel_to_track,stack_to_track,i))
     roi = OvalRoi(roi_x, roi_y, roi_w, roi_h)
@@ -79,7 +141,7 @@ for i in range(start_frame, n_frames+1):
     stats = ImageStatistics.getStatistics(ip1, ImageStatistics.CENTER_OF_MASS, cal)
     x=cal.getRawX(stats.xCenterOfMass)
     y=cal.getRawY(stats.yCenterOfMass)
-    means1.append(stats.mean)
+    means1.append(stats.mode)
     c1x.append(x)
     c1y.append(y)
     
@@ -90,19 +152,20 @@ for i in range(start_frame, n_frames+1):
     stats = ImageStatistics.getStatistics(ip2, ImageStatistics.CENTER_OF_MASS, cal)
     x=cal.getRawX(stats.xCenterOfMass)
     y=cal.getRawY(stats.yCenterOfMass)
-    means2.append(stats.mean)
+    means2.append(stats.mode)
     c2x.append(x)
     c2y.append(y)
 
-    ip2=ip.duplicate()
-    ip2.setColor(ip.maxValue())
-    ip2.draw(roi)
-    stack2.addSlice(ip2)
+    if showTrackFlag:
+        ip2=ip.duplicate()
+        ip2.setColor(ip.maxValue())
+        ip2.draw(roi)
+        stack2.addSlice(ip2)
 
-    
-im2 = ImagePlus(title+'_Processed', stack2)
-im2.setCalibration(cal)
-im2.show()
+if showTrackFlag:    
+    im2 = ImagePlus(title+'_Processed', stack2)
+    im2.setCalibration(cal)
+    im2.show()
 
 IJ.run("Clear Results")
 
@@ -118,7 +181,9 @@ for i in range(len(means1)):
 	rt.addValue('Ch2.Center.of.mass.Y', c2y[i])
 
 rt.disableRowLabels()
-rt.show(title)
+
+if showResultsFlag:
+    rt.show(title)
 
 maxc1=max(means1)
 maxc2=max(means2)
@@ -129,7 +194,7 @@ else:
 
 time=range(0,len(means1))
 plot = Plot("Traced intensity curve for " + imp.getTitle(), "Time", "Mean intensity", [], [])
-plot.setLimits(1, n_frames+1, 0, plotlim );
+plot.setLimits(1, stop_frame+1, 0, plotlim );
 plot.setLineWidth(2)
 
 plot.setColor(Color.GREEN)
@@ -139,5 +204,7 @@ plot.setColor(Color.RED)
 plot.addPoints(time, means2, Plot.LINE)
  
 plot.setColor(Color.black)
-plot_window =  plot.show()
+
+if showPlotFlag:
+    plot_window =  plot.show()
 
