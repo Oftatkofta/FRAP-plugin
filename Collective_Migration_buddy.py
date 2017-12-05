3e8cd3adbcc27648a626896ab02bdf4546cadc39
Jens Eriksson <jenseri@gmail.com>
2017-12-05 23:12:47 +0100

    Licence boiler and indentation

--- Collective_Migration_buddy.py
+++ Collective_Migration_buddy.py
@@ -1,4 +1,28 @@
-#import ij.gui
+# This file is ment to be placed in the /plugins/ directory of FIJI and is ment
+# to be run from within the plugins menu.
+#
+# Copyright 2016 Jens Eriksson 
+
+# Permission is hereby granted, free of charge, to any person obtaining a 
+# copy of this software and associated documentation files (the 
+# "Software"), to deal in the Software without restriction, including 
+# without limitation the rights to use, copy, modify, merge, publish, 
+# distribute, sublicense, and/or sell copies of the Software, and to 
+# permit persons to whom the Software is furnished to do so, subject to 
+# the following conditions: 
+
+# The above copyright notice and this permission notice shall be included 
+# in all copies or substantial portions of the Software. 
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+
+
 from ij.plugin import ZProjector, Duplicator, HyperStackConverter
 from ij import WindowManager as WindowManager
 from ij import IJ, ImagePlus, ImageStack
@@ -8,9 +32,23 @@ import math
 
 
 def setupDialog(imp):
+    """
+    Creates a GenericDialog object where the relevant settings for the plugin
+    can be accessed and set through a convenient GUI
+
+    Args:
+        imp: ij.ImagePlus object, usually the currently active window.
+
+    Returns:
+        A GenericDialog Object containig all the desired settings for the
+        analysis
+    """
 
     gd = GenericDialog("Collective migration buddy options")
-    gd.addMessage("Collective migration buddy 2.0, you are analyzing: "+imp.getTitle())
+    
+    gd.addMessage("Collective migration buddy 2.0, you are analyzing: "+
+                  imp.getTitle())
+    
     calibration = imp.getCalibration()
 
     if (calibration.frameInterval > 0):
@@ -21,40 +59,51 @@ def setupDialog(imp):
         default_interval = 8
         default_timeunit = "min"
 		
-    gd.addNumericField("Frame interval:", default_interval, 2)  # show 2 decimals    
+    gd.addNumericField("Frame interval:", default_interval, 2)  #show 2 decimals    
     gd.addCheckbox("Do you want to use a gliding window?", True)
-    gd.addCheckbox("Project hyperStack? (defaluts to projecting current channel only)", False)
-    gd.addStringField("time unit",default_timeunit, 3)
-    gd.addSlider("Start compacting at frame:", 1, imp.getNFrames(), 1)
-    gd.addSlider("Stop compacting at frame:", 1, imp.getNFrames(), imp.getNFrames())
-    gd.addNumericField("Number of frames to project in to one:", 3, 0)  # show 0 decimals
+    gd.addCheckbox("Project hyperStack? "+
+                   "(defaluts to projecting current channel only)", False)
     
+    gd.addStringField("Time unit",default_timeunit, 3)
+    gd.addSlider("Start projecting at frame:", 1, imp.getNFrames(), 1)
+    gd.addSlider("Stop projecting at frame:", 1, imp.getNFrames(),
+                 imp.getNFrames())
+                 
+    gd.addNumericField("Number of frames to project in to one:", 3, 0)
     
-    gd.addChoice('Method to use for frame projection:', methods_as_strings, methods_as_strings[5])
+    gd.addChoice("Method to use for frame projection:", methods_as_strings,
+                 methods_as_strings[5])
     
     gd.showDialog()  
 	  
     if gd.wasCanceled():  
-        IJ.log("User canceled dialog!")  
+        IJ.log("User canceled dialog!") 
         return
 
     return gd
 
-#Start by getting the active image window and get the current active channel and other stats
+#Start by getting the active image window
 imp = WindowManager.getCurrentImage()
+
 cal = imp.getCalibration()
 nSlices = 1 #TODO fix this in case you want to do Z-stacks
 title = imp.getTitle()
 current_channel = imp.getChannel()
-#zp = ZProjector(imp)
 
-#Make a dict containg method_name:const_fieled_value pairs for the projection methods
-methods_as_strings=['Average Intensity', 'Max Intensity', 'Min Intensity', 'Sum Slices', 'Standard Deviation', 'Median']
-methods_as_const=[ZProjector.AVG_METHOD, ZProjector.MAX_METHOD, ZProjector.MIN_METHOD, ZProjector.SUM_METHOD, ZProjector.SD_METHOD, ZProjector.MEDIAN_METHOD]
+#projection methods in human readable form
+methods_as_strings=['Average Intensity', 'Max Intensity', 'Min Intensity',
+                    'Sum Slices', 'Standard Deviation', 'Median']
+
+#projection methods in machine readable form                    
+methods_as_const=[ZProjector.AVG_METHOD, ZProjector.MAX_METHOD,
+                  ZProjector.MIN_METHOD, ZProjector.SUM_METHOD,
+                  ZProjector.SD_METHOD, ZProjector.MEDIAN_METHOD]
+
 medthod_dict=dict(zip(methods_as_strings, methods_as_const))
 
 # Run the setupDialog, read out and store the options
 gd=setupDialog(imp)
+
 frame_interval = gd.getNextNumber()
 time_unit = gd.getNextString()
 glidingFlag = gd.getNextBoolean()
@@ -65,17 +114,17 @@ cal.frameInterval = frame_interval
 cal.setTimeUnit(time_unit)
 imp.setCalibration(cal)
 
-
 start_frame = int(gd.getNextNumber())
 stop_frame = int(gd.getNextNumber())
 
 #If a subset of the image is to be projected, these lines of code handle that
 if (start_frame > stop_frame):
     IJ.showMessage("Start frame > Stop frame, can't go backwards in time!")
-    raise RuntimeException("Start frame > Stop frame!")
+    raise Exception("Start frame > Stop frame!")
 
 if ((start_frame != 1) or (stop_frame != imp.getNFrames())):
-    imp = Duplicator().run(imp, 1, nChannels, 1, nSlices, start_frame, stop_frame)
+    imp = Duplicator().run(imp, 1, nChannels, 1, nSlices, start_frame,
+                           stop_frame)
     
  
 no_frames_per_integral = int(gd.getNextNumber())
@@ -83,14 +132,18 @@ no_frames_per_integral = int(gd.getNextNumber())
 #the doHyperstackProjection method can't project past the end of the stack
 if hyperstackFlag: 
 	total_no_frames_to_project=imp.getNFrames()-no_frames_per_integral
-#the doProjection method can project past the end, it just adds black frames at the end
-#When not projecting hyperstacks, just copy the current active channel from the active image
+#doProjection method can project past the end, it adds black frames at the end
+
+#if not projecting hyperstacks, just copy the current active channel
 else:
 	total_no_frames_to_project=imp.getNFrames()
-	imp = Duplicator().run(imp, current_channel, current_channel, 1, nSlices, start_frame, stop_frame)
+	
+    imp = Duplicator().run(imp, current_channel, current_channel, 1, nSlices,
+                           start_frame, stop_frame)
 
 #The Z-Projection magic happens here through a ZProjector object
 zp = ZProjector(imp)
+
 projection_method=gd.getNextChoice()
 chosen_method=medthod_dict[projection_method]
 zp.setMethod(chosen_method)
